27cfe2ef2f63569699567334e36d3645d6c7d8d6
Lucas Basilio Leite <47511187811@info.local>
2019-04-18 08:31:18 -0300

    Alterações para melhor compatibilidade

--- Script/Script_InsertManual.sql
+++ Script/Script_InsertManual.sql
@@ -5,11 +5,11 @@ INSERT INTO ESPECIALIZACAO VALUES('Acupuntura'), ('Anestesiologia'), ('Angiologi
 ('Cirurgia Torácica'), ('Cirurgia Vascular'), ('Dermatologia'), ('Radioterapia'), ('Urologia'), ('Pediatria'),
 ('Psiquiatria');
 
-INSERT INTO USUARIOS VALUES('Ro.O.Mais.132@gmail.com', '132', 1, 01/01/2001), 
-('Rodolfinho.gatao132@gmail.com','132',1, 01/01/2001),
-('Rogerin.murchaosacolademercado','132',2, 01/01/2001),
-('feehzika@amaistoop.gmail.com','132',2, 01/01/2001),
-('tiao3leg@gmail.com','132', 1, 01/01/2001);
+INSERT INTO USUARIOS VALUES('Roberval', 'Ro.O.Mais.132@gmail.com', '132', 1, 01/01/2001), 
+('Rodolfo', 'Rodolfinho.gatao132@gmail.com','132',1, 01/01/2001),
+('Rogério', 'Rogerin.murchaosacolademercado','132',2, 01/01/2001),
+('Fernanda', 'feehzika@amaistoop.gmail.com','132',2, 01/01/2001),
+('Tião', 'tiao3leg@gmail.com','132', 1, 01/01/2001);
 
 INSERT INTO PACIENTES VALUES ('Rodolfo', '512345347', '47512346523', 2),
 ('Roberval','123452341','42365748765',1), ('Tião', '345678769', '23455565633', 5);
@@ -18,4 +18,4 @@ INSERT INTO PROGRESSO VALUES('Agendada'),('Em andamento'),('Concluida'),('Cancel
 
 INSERT INTO MEDICOS VALUES('Rogério', 15, '43245', '31.248.899/0001-90', 3), ('Fernanda', 14, '24243', '96.093.759/0001-35', 4);
 
-INSERT INTO CONSULTA VALUES(3, 9, 02/02/2002, 1, 'Paciente terminal'), (1, 10, 01/01/2001, 2, 'Receitar xarope');
+INSERT INTO CONSULTA VALUES(3, 1, 02/02/2002, 1, 'Paciente terminal'), (1, 2, 01/01/2001, 2, 'Receitar xarope');
